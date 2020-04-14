a04f71b7625005ade669bcd4db547712df951b01
Yannick <ydjoumbi@gmail.com>
2020-04-14 20:15:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    programmer_hash.update([:alan_kay][:known_for]="GUI" )
+    programmer_hash[:alan_kay][:known_for]="GUI" 
 
 end
 
